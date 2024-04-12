31f76eb35631b2aa089d6666b3fa4385127d98c2
ThiRamos01 <thiagojramos01@gmail.com>
2024-04-11 23:22:19 -0300

    Adicionando posts e estilizando home

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,7 @@
 <?php
     include_once("helpers/url.php");
+    include_once("data/posts.php");
+    include_once("data/categories.php");
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -7,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog</title>
-    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
